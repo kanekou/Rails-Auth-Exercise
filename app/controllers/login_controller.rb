e1a589feb8beeafad0f6e3ad3e1996e5bb9dbc5f
kanekou <e165726@ie.u-ryukyu.ac.jp>
2020-09-06 18:10:48 +0900

    Add bcypt

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,8 +1,8 @@
 class LoginController < ApplicationController
   def login
-    login_user = User.find_by(name: params[:name], password: params[:password])
-    if !login_user.nil?
-      render plain: login_user.token, status: 200
+    user = User.find_by(name: params[:name])
+    if user.authenticate(params[:password])
+      render plain: user.token, status: 200
     else
       render plain: 'no auth', status: 403
     end
